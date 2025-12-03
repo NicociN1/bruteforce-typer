5fce53b24125f3ffbac08d45b8e20f9e45155139
石神 大輝 <nicon@NMacIT.local>
2025-12-04 00:29:38 +0900

    feat: Google Search Console追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ const dotGothic = DotGothic16({
 export const metadata: Metadata = {
   title: "BruteForce-Typer | タイピングゲーム",
   description: "パスワードのハッキングをテーマにしたタイピングゲーム。アシスタントのAIと一緒に悪事を働いているハッカーのシステムを乗っ取ろう！",
+  verification: {
+    google: 'J8TVVkURfTF1YRhn5Z76YgxjTOI22UB83ziG3ZT3jW8'
+  }
 };
 
 export default function RootLayout({
